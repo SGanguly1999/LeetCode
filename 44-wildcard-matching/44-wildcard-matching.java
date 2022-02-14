1ba5084838608b9a0385a5f65747a78d41ba8bfc
Soumyadeep Ganguly <42171790+SGanguly1999@users.noreply.github.com>
2022-02-14 10:16:04 +0530

    Time: 31 ms (57.59%), Space: 49.1 MB (20.96%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        s = " " + s;
+        p= " " + p;
+        int s_l=s.length();
+        int p_l=p.length();
+        boolean mat[][]=new boolean[p_l][s_l];
+        for(int i=0;i<p_l;i++) {
+            char c = p.charAt(i);
+            for(int j=0;j<s_l;j++) {
+            char d = s.charAt(j);
+            if(i==0 && j==0)
+                mat[i][j]= true;
+                else {
+                    if(c == '?' && j!=0 && mat[i-1][j-1] == true)
+                        mat[i][j]= true;
+                    else if(c == d && j!=0 && mat[i-1][j-1] == true)
+                        mat[i][j] = true;
+                    else if(c == '*')
+                    {
+                      if(j==0 && mat[i-1][j] == true)
+                          mat[i][j] = true;
+                        else {
+                            if(j!=0 && (mat[i][j-1] == true || mat[i-1][j] == true))
+                                mat[i][j] = true;
+                        }
+                }
+           
+            }
+        }
+        
+    }
+        return mat[p_l-1][s_l-1];
+}
+}
