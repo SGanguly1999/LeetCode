9aa8c4eb165e9676d1089e1ead88256fdbbaf41c
Soumyadeep Ganguly <42171790+SGanguly1999@users.noreply.github.com>
2022-02-20 19:31:11 +0530

    Time: 1 ms (56.15%), Space: 45.6 MB (25.58%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -1,58 +1,70 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    static ListNode currHead;
-    static ListNode currEnd,currPrevEnd;
-    static int countNodes;
     public ListNode reverseKGroup(ListNode head, int k) {
-       countNodes = 0;
-        ListNode newHead = null;
-       currHead =currEnd = null;
-       reverseKelem(head,k);
-       newHead = currHead;
-       while(currEnd.next!=null) {
-       reverseKelem(currEnd.next, k);
-       }
-       if(countNodes%k!=0) {
-           currEnd = currPrevEnd;
-           reverseKelem(currHead, countNodes%k);
-       }
-        return newHead;
-    }
-    public void reverseKelem(ListNode head,int k) {
-        int count = 0;
-        ListNode currNode,currPrev;
-        currNode = currPrev = null;
-        currNode = head;
-        currPrev = currNode;
-        currNode = head.next;
-        count++;
-         if(currNode == null) {
-             currHead = currEnd = head;
-            return;
-         }
-        while(count != k && currNode != null) {
-            count++;
-            ListNode currNext = currNode.next;
-            currNode.next = currPrev;
-            currPrev = currNode;
-            currNode = currNext;
+        int length = 0;
+        ListNode curr = head;
+        while(curr != null){
+            length++;
+            curr = curr.next;
         }
-        currHead = currPrev;
-        if(currEnd!=null)
-        currEnd.next = currHead;
-        currPrevEnd = currEnd;
-        currEnd = head;
-        currEnd.next = currNode;
-        countNodes+=count;
-        //System.out.println(currHead.val +" "+currEnd.val);
+
+        for(int i=1, j=k; j<=length; i+=k, j+=k){
+            head = reverseBetween(head, i, j);
+        }
+        
+        return head;
     }
+    
+    private ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode start = head, end = head;
+        ListNode startPrev = null, endNext = null;
+
+        //get start and end nodes
+        int i = 1;
+        while (i < right) {
+            if (i < left) {
+                startPrev = start;
+                start = start.next;
+            }
+
+            if (i < right) {
+                end = end.next;
+            }
+
+            i++;
+        }
+        
+        //save end next node
+        endNext = end.next;
+        end.next = null;
+
+        //reverse the sub linkedlist
+        ListNode reversedHead = reverse(start, end);
+
+        //if sub is the start of the linkedlist
+        if (startPrev == null) {
+            head = reversedHead;
+        } 
+        // connect start previous with the new reverse head
+        else { 
+            startPrev.next = reversedHead;
+        }
+
+        //connect the end of the sub linkedlist to the saved end.next
+        start.next = endNext;
+
+        //return beginning of the linked list 
+        return head;
+    }
+
+    private ListNode reverse(ListNode start, ListNode end) {
+        ListNode curr = start, prev = null;
+        while (curr != null) {
+            ListNode nextTmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTmp;
+        }
+        return prev;
+    }
+
 }
