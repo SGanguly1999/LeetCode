c54e31bfdaeff20ee055818f6d8b5e9ba6a2ae23
Soumyadeep Ganguly <42171790+SGanguly1999@users.noreply.github.com>
2022-08-01 23:05:29 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (6.09%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,9 +2,9 @@ class Solution {
     long store[];
     public int uniquePaths(int m, int n) {
         int k = Math.max(n,m);
-        long val =1;
-        for(int i=k,c=1;i<=(m+n-2);i++,c++) {
-            val=val * i/c;
+        double val =1;
+        for(double i=k,c=1;i<=(m+n-2);i++,c++) {
+            val=(val * i)/c;
             //val/=c;
         }
         return (int)val;
