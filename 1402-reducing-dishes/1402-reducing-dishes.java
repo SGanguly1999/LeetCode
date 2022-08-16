de4c4f103fe702bbbc60911972612fd7f76b7245
Soumyadeep Ganguly <42171790+SGanguly1999@users.noreply.github.com>
2022-08-16 22:03:15 +0530

    Time: 2 ms (94.68%), Space: 41.9 MB (80.06%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
-        int len = satisfaction.length;
         int result=0;
         int sum = 0;
         for (int i = satisfaction.length - 1; i >= 0; i--) {
